b93295d58a0697598b23d080db3f85f297f328d8
mainumairkhalid <computeexpert@gmail.com>
2023-06-28 10:42:00 +0500

    working

--- src/pages/Business.js
+++ src/pages/Business.js
@@ -1,13 +1,34 @@
 import React from 'react'
 
 function Business() {
-  return (
-    <>
-        <div>
-            code
-        </div>
-    </>
-  )
+    return (
+        <>
+            <div className="pl-10 pt-10 page-section">
+                <div className="pt-0">
+                    <div className=" max-w-2xl">
+                        <div className="max-w-lg">
+                            <h1 className="text-xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+                                Dream it. Build it. Grow it.🎉
+                            </h1>
+                            <p className="mt-1 text-sm leading-8 text-gray-600">
+                                Welcome to your <span style={{ color: "#006AFF" }} className="font-bold">Zaviago/OS</span> v.1.4.88
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="border border-[#EFEFEF] pl-16 pt-12">
+                    <div>
+                        <p className="font-inter text-xs font-normal" >zaviago 2023</p>
+                        <h3 className="font-inter text-2xl font-semibold w-56 mt-4">Connect Business Intelligent</h3>
+                    </div>
+                    <div>
+                        <p>Facilitate payments and pay out sellers or service providers.</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Business
