6b37322e1ee8f3d21f81630d7b136e7e11b422ce
MarkSuttirak <suttirak.ch@zaviago.com>
2023-06-29 09:47:49 +0700

    Commit Codes

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -59,7 +59,9 @@ const Sidebar = () => {
                 src={Logo}
                 alt="Your Company"
               />
-              <h1>Test</h1>
+              <h1 className="sidebar-btn close-sidebar-mobile" onClick={closeSidebar}>
+                <svg xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+              </h1>
             </div>
             <nav className="mt-5 flex-1 space-y-1 bg-white px-4 pt-4" aria-label="Sidebar">
 
@@ -118,7 +120,9 @@ const Sidebar = () => {
         </div>
         
         <header className="header-mobile">
-          <h1 onClick={openSidebar} style={{cursor:"pointer"}}>Test</h1>
+          <h1 onClick={openSidebar} style={{cursor:"pointer"}}>
+          <svg xmlns="http://www.w3.org/2000/svg" height="0.9em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+          </h1>
         </header>
       </>
     )
