7cbb57931e7d3758e0437dea6bc08e248efd0a61
mianumairkhalid <135826725+mianumairkhalid@users.noreply.github.com>
2023-07-04 10:35:09 +0500

    update popup

--- src/components/popup.js
+++ src/components/popup.js
@@ -3,13 +3,13 @@ import React from 'react'
 function popup() {
     return (
         <>
-            <div className='w-[511px] h-[363px] bg-white p-8'>
+            <div>
                 <h3 className='font-inter font-semibold text-[#333333] text-base'>เพิ่มสมาชิก</h3>
                 <div className='mt-5'>
                     <p className='font-sukhumvit font-semibold text-[#333333] text-[13px]'> ส่งคำเชิญสมาชิกเข้าทีมผ่านอีเมลหรือลิงก์ </p>
                     <div>
-                        <div className='bg-[#F4F5F6] rounded-md'>
-                            <input type="text" placeholder='กรุณากรอกอีเมลของสมาชิกในทีม' className='py-2 px-4' />
+                        <div className='bg-[#F4F5F6] rounded-md mt-2'>
+                            <input type="text" placeholder='กรุณากรอกอีเมลของสมาชิกในทีม' className='py-1 px-4 m-1 bg-[#F4F5F6]' />
                         </div>
                     </div>
                 </div>
