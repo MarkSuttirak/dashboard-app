611f8e11c1df2e516804a23b2790ff6f4b953e6b
mianumairkhalid <135826725+mianumairkhalid@users.noreply.github.com>
2023-07-04 09:58:00 +0500

    Update popup

--- src/components/popup.js
+++ src/components/popup.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function popup() {
   return (
-    <div>popup</div>
+    <>
+        <div className='w-[511px] h-[363px] bg-white'>
+            <h3 className='font-inter font-semibold text-[#333333] text-base'>เพิ่มสมาชิก</h3>
+        </div>
+    </>
   )
 }
 
