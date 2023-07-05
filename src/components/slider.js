c0d37e18bcb31b9fa37614fdf9093a2ab5f41d75
mianumairkhalid <computeexpert@gmail.com>
2023-07-05 10:05:53 +0500

    Slider and other Changes

--- src/components/slider.js
+++ src/components/slider.js
@@ -60,9 +60,9 @@ const SliderComponent = ({ images, className }) => {
       </Slider>
       
     </div>
-    <div className='text-right mt-2 mr-2'>
+    {/* <div className='text-right mt-2 mr-2'>
       <button className='border border-[#DCDFE9] w-[100px] h-[32px] cursor-default rounded-2xl font-inter font-medium text-sm text-[#32325D]'>+ 2 more</button>
-    </div>
+    </div> */}
 </>
   );
 };
