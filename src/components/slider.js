2c3bb0024a79ee036cf5f12f80bf735b5f1a6ed0
mianumairkhalid <135826725+mianumairkhalid@users.noreply.github.com>
2023-07-05 08:25:20 +0500

    Update slider.js

--- src/components/slider.js
+++ src/components/slider.js
@@ -61,7 +61,7 @@ const SliderComponent = ({ images, className }) => {
       
     </div>
     <div className='text-right mt-2 mr-2'>
-      <button className='border border-[#DCDFE9] w-[100px] h-[32px] rounded-2xl font-inter font-medium text-sm text-[#32325D]'>+ 2 more</button>
+      <button className='border border-[#DCDFE9] w-[100px] h-[32px] cursor-default rounded-2xl font-inter font-medium text-sm text-[#32325D]'>+ 2 more</button>
     </div>
 </>
   );
