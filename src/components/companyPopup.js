eb2c54f11f86fa582374859f0083ae33c2a93b55
mianumairkhalid <computeexpert@gmail.com>
2023-07-10 15:03:20 +0500

    teampage changes

--- src/components/companyPopup.js
+++ src/components/companyPopup.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import chevronDown from '../img/chevron-down.png'
-import closeIcon from '../img/closeIcon.png'
+import closeIcon from '../img/closeIcon.svg'
 
 function CompanyPopup({ closePopUp2 }) {
 
@@ -14,9 +14,45 @@ const [input, setInput] = useState('')
         setInput(event.target.value)
     }
 
-    const handleFormSubmit = ()=>{
+    const [email, setEmail] = useState('');
+    const [borderColor, setBorderColor] = useState('#F4F5F6');
+    const [error, setError] = useState('');
+    const handleEmailChange = (event) => {
+        const { value } = event.target;
+        setEmail(value);
+        setError('');
+    };
 
-    }
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailPattern.test(email)) {
+            setError('กรุณากรอกอีเมลที่ถูกต้อง');
+            setBorderColor('#EF4444');
+            return;
+        } else {
+            setBorderColor('#DDDDDD');
+            setError('');
+        }
+    };
+
+    const handleEmailFocus = () => {
+        console.log(error.length)
+        if (error.length > 0) {
+            setBorderColor('#DDDDDD');
+        } else if (email.length === '') {
+            setBorderColor('none');
+        } else {
+            setBorderColor('#DDDDDD');
+        }
+        setError('');
+    };
+
+    const handleEmailBlur = () => {
+        setBorderColor('#F4F5F6');
+    };
 
     return (
         <>
@@ -30,14 +66,15 @@ const [input, setInput] = useState('')
                 <div className='mt-4'>
                         <p className='font-sukhumvit font-medium text-[#333333] text-[13px]'> กรอกชื่อที่คุณต้องการแก้ไข </p>
                         <div className=''>
-                            <div className={`bg-[#F4F5F6] rounded-md mt-2 pl-4 px-1 h-[32px]`}>
+                            <div className={`bg-[#F4F5F6] rounded-md mt-2 px-1 h-[32px]`} style={{ border: `2px solid ${borderColor}` }}>
                                 <div>
-                                    <input type="text" value={input} placeholder='Zaviago' onChange={handleInputChange} className={`bg-[#F4F5F6] font-sukhumvit font-medium text-[12px] placeholder-gray-500 focus:outline-none h-[32px] w-full`} />
+                                    <input type="text" value={email} onBlur={handleEmailBlur} onFocus={handleEmailFocus} onChange={handleEmailChange} placeholder='Zaviago' className={`bg-[#F4F5F6] font-sukhumvit font-medium text-[12px] placeholder-[9CA3AF] pl-4 focus:outline-none h-[17px] w-full`} />
                                 </div>
+                                {error && (<p className='font-normal text-[12px] mt-2 font-sukhumvit text-[#EF4444]'> {error} </p>)}
                             </div>
                             <div className='mt-5 text-right'>
-                            <button onClick={handleClosePopup} className='sm:w-[75px] w-[67px] h-[32px] cursor-pointer ml-4 bg-[#F3F3F3] text-[#333333] rounded-md shadow-md font-sukhumvit font-bold text-[13px]'>ยกเลิก</button>
-                            <button onClick={handleClosePopup} className='sm:w-[75px] w-[67px] h-[32px] cursor-pointer ml-4 bg-[#0099FF] text-white rounded-md shadow-md font-sukhumvit font-bold text-[13px]'>บันทึก</button>
+                            <button onClick={handleClosePopup} className='sm:w-[75px] w-[67px] h-[32px] cursor-pointer ml-4 bg-[#F3F3F3] text-[#333333] rounded-md font-sukhumvit font-bold text-[13px]'>ยกเลิก</button>
+                            <button  style={{boxShadow: 'rgba(0, 153, 255, 0.2) 0px 4px 4px'}}  className='sm:w-[75px] w-[67px] h-[32px] cursor-pointer ml-4 bg-[#0099FF] text-white rounded-md font-sukhumvit font-bold text-[13px]'>บันทึก</button>
                             </div>
                         </div>
                     </div>
