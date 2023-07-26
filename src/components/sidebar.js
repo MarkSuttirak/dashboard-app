33cebda82897270ca398f3ff2789cb084e2b735f
MarkSuttirak <suttirak.ch@zaviago.com>
2023-07-26 15:32:04 +0700

    Commit Codes

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
-import Logo from "../img/logo.svg";
+import Logo from "../img/Copilot.svg";
 import dashboardImg from "../img/dashboard.svg";
 import appsImg from "../img/apps.svg";
 import teamsImg from "../img/teams.svg";
+import giftImg from "../img/giftImg.svg";
 import billingImg from "../img/billingImg.svg";
 import settingsImg from "../img/settingsImg.svg";
 import { Link, useLocation } from "react-router-dom";
@@ -40,6 +41,7 @@ const Sidebar = ({ loadingLogo }) => {
     { name: 'Dashboard', icon: dashboardImg, href: '/', current: active === '/' ? true : false },
     { name: 'Apps', icon: appsImg, href: '/apps', count: 3, current: active === '/apps' ? true : false },
     { name: 'Teams', icon: teamsImg, href: '/teams', count: 4, current: active === '/teams' ? true : false },
+    { name: 'Gift & Privilege', icon: giftImg, href: '#', current: active === "#" ? true : false, active: active },
     { name: 'Billing', icon: billingImg, href: '#', current: active === "#" ? true : false, active: active },
     { name: 'Setting', icon: settingsImg, href: '#', count: 12, current: active === "/settings" || active === "/change-domain" ? true : false, active: active },
   ]
@@ -108,13 +110,16 @@ const Sidebar = ({ loadingLogo }) => {
       <div className="w-full h-full fixed z-40 bg-gray-500" id="sidebar-overlay" onClick={closeSidebar}></div>
       <div className="flex min-h-0 flex-1 flex-col border-r border-gray-200 bg-white fixed h-screen" id="sidebar">
         <div className="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
-          <div className="flex flex-shrink-0 justify-between items-center px-4 columns-2">
+          <div className="flex flex-shrink-0 items-center px-4 columns-2 gap-x-3">
             {!loadingLogo ? (
-              <img
-                className="w-auto"
-                src={Logo}
-                alt="Your Company"
-              />
+              <>
+                <img
+                  className="w-auto"
+                  src={Logo}
+                  alt="Your Company"
+                />
+                <h1 className="calsans text-[#1D2D35] text-[19px]">zaviago<span className="text-xs calsans">.com</span></h1>
+              </>
             ) : (
               <div className="animate-pulse">
                 <div className="bg-[#F3F3F3] w-[54px] aspect-square rounded-lg"></div>
@@ -123,10 +128,6 @@ const Sidebar = ({ loadingLogo }) => {
 
             {!loadingLogo ? (
               <div>
-                <h1 className="sidebar-btn search-btn" onClick={openSearch}>
-                  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
-                </h1>
-
                 <h1 className="sidebar-btn close-sidebar-mobile" onClick={closeSidebar}>
                   <svg xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" /></svg>
                 </h1>
@@ -138,15 +139,8 @@ const Sidebar = ({ loadingLogo }) => {
             )}
           </div>
 
-          <div className="flex flex-shrink-0 justify-between items-center px-4 columns-2 search-btn-sec mt-4 -mb-2">
-            <h1 className="sidebar-btn search-btn-mobile" onClick={openSearch}>
-              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
-            </h1>
-          </div>
-
           {!loadingLogo ? (
             <nav className="mt-5 flex-1 space-y-1 bg-white px-4 pt-4" aria-label="Sidebar">
-              <h4 className="text-sm font-semibold" style={{ fontFamily: "Inter", color: "#1F272E" }}>Business Apps</h4>
               {navigation.map((item) => (
                 <Link
                   key={item.name}
