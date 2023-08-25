a155e5cd414b612edd91192da274a9b855a06787
MarkSuttirak <suttirak.ch@zaviago.com>
2023-08-25 19:13:49 +0700

    Update connectedApps.js

--- src/pages/connectedApps.js
+++ src/pages/connectedApps.js
@@ -85,72 +85,63 @@ const ConnectedApps = () => {
           </div>
 
           <div className="mt-8 flex flex-col">
-            <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-              <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-                <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                  <table className="min-w-full divide-y divide-gray-300">
-                    <thead className="bg-gray-50">
-                      <tr>
-                        <th scope="col" className="table-head-text first-col">
-                          Company
-                        </th>
-                        <th scope="col" className="table-head-text">
-                          Status
-                        </th>
-                        <th scope="col" className="table-head-text">
-                          About
-                        </th>
-                        <th scope="col" className="table-head-text">
-                          Usage
-                        </th>
-                        <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                          <span className="sr-only">Edit</span>
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody className="divide-y divide-gray-200 bg-white">
-                      {apps.map((app) => (
-                        <tr key={app.companytitle}>
-                          <td className="whitespace-nowrap py-4 pr-3 text-sm pl-6">
-                            <div className="flex items-center">
-                              <div className="h-10 w-10 flex-shrink-0">
-                                <img className="h-10 w-10 rounded-full" src={app.image} alt="" />
-                              </div>
-                              <div className="ml-4">
-                                <div className="font-medium text-gray-900">{app.companytitle}</div>
-                                <div className="text-gray-500">{app.companydesc}</div>
-                              </div>
-                            </div>
-                          </td>
-                          <td className="whitespace-nowrap py-4 text-sm text-gray-500">
-                            <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-                              Active
-                            </span>
-                          </td>
-                          <td className="whitespace-nowrap py-4 text-sm text-gray-500">
-                            <div className="table-desc">{app.abouttitle}</div>
-                            <div className="table-desc">{app.aboutdesc}</div>
-                          </td>
-                          <td className="whitespace-nowrap py-4 text-sm text-gray-500">
-                            <div className="w-full bg-gray-200 rounded-full h-2 dark:bg-gray-700">
-                              <div className="bg-blue-600 h-2 rounded-full dark:bg-blue-500" style={{ width: `${app.usage}%` }}></div>
-                            </div>
-                          </td>
-                          <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 flex gap-x-2 items-center h-20">
-                            <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                              <TrashIcon className='w-5 h-5'/>
-                            </a>
-                            <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                              <PencilIcon className='w-5 h-5'/>
-                            </a>
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
+            <table className="table-overview">
+              <thead className="table-head">
+                <tr>
+                  <th scope="col" className="table-head-text first-col">
+                    Company
+                  </th>
+                  <th scope="col" className="table-head-text">
+                    Status
+                  </th>
+                  <th scope="col" className="table-head-text">
+                    About
+                  </th>
+                  <th scope="col" className="table-head-text">
+                    Usage
+                  </th>
+                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                    <span className="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200 bg-white">
+                {apps.map((app) => (
+                  <tr key={app.companytitle}>
+                    <td className="whitespace-nowrap py-4 pr-3 text-sm pl-6">
+                      <div className="flex items-center">
+                        <div className="h-10 w-10 flex-shrink-0">
+                          <img className="h-10 w-10 rounded-full" src={app.image} alt="" />
+                        </div>
+                        <div className="ml-4">
+                          <div className="subheading">{app.companytitle}</div>
+                          <div className="table-desc">{app.companydesc}</div>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="whitespace-nowrap py-4 text-sm text-gray-500">
+                      <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+                        Active
+                      </span>
+                    </td>
+                    <td className="whitespace-nowrap py-4 text-sm text-gray-500">
+                      <div className="subheading">{app.abouttitle}</div>
+                      <div className="table-desc">{app.aboutdesc}</div>
+                    </td>
+                    <td className="whitespace-nowrap py-4 text-sm text-gray-500">
+                      <div className="w-full bg-gray-200 rounded-full h-2 dark:bg-gray-700">
+                        <div className="bg-blue-600 h-2 rounded-full dark:bg-blue-500" style={{ width: `${app.usage}%` }}></div>
+                      </div>
+                    </td>
+                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                      <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                        Edit
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
             <nav
                 className="flex items-center justify-between py-4"
                 aria-label="Pagination"
