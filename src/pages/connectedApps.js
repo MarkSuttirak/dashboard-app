f41114554e1d7672f7f538a6b4b03bd63c514e65
MarkSuttirak <suttirak.ch@zaviago.com>
2023-08-25 18:17:53 +0700

    Update connectedApps.js

--- src/pages/connectedApps.js
+++ src/pages/connectedApps.js
@@ -1,6 +1,6 @@
 import HeaderIntegration from '../components/headerIntegration'
 import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/20/solid'
-import { CursorArrowRaysIcon, EnvelopeOpenIcon, UsersIcon } from '@heroicons/react/24/outline'
+import { CursorArrowRaysIcon, EnvelopeOpenIcon, UsersIcon, TrashIcon, PencilIcon } from '@heroicons/react/24/outline'
 
 const stats = [
   { id: 1, name: 'Total Subscribers', stat: '71,897', icon: UsersIcon, change: '122', changeType: 'increase' },
@@ -56,15 +56,12 @@ const ConnectedApps = () => {
               {stats.map((item) => (
                 <div
                   key={item.id}
-                  className="relative overflow-hidden rounded-lg bg-white px-4 pt-5 pb-12 shadow sm:px-6 sm:pt-6"
+                  className="relative overflow-hidden rounded-lg bg-white border border-[#F2F2F2] px-6 py-6"
                 >
                   <dt>
-                    <div className="absolute rounded-md bg-indigo-500 p-3">
-                      <item.icon className="h-6 w-6 text-white" aria-hidden="true" />
-                    </div>
-                    <p className="ml-16 truncate text-sm font-medium text-gray-500">{item.name}</p>
+                    <p className="truncate text-sm font-medium text-gray-500">{item.name}</p>
                   </dt>
-                  <dd className="ml-16 flex items-baseline pb-6 sm:pb-7">
+                  <dd className="flex items-baseline">
                     <p className="text-2xl font-semibold text-gray-900">{item.stat}</p>
                     <p
                       className={classNames(
@@ -81,14 +78,6 @@ const ConnectedApps = () => {
                       <span className="sr-only"> {item.changeType === 'increase' ? 'Increased' : 'Decreased'} by </span>
                       {item.change}
                     </p>
-                    <div className="absolute inset-x-0 bottom-0 bg-gray-50 px-4 py-4 sm:px-6">
-                      <div className="text-sm">
-                        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                          {' '}
-                          View all<span className="sr-only"> {item.name} stats</span>
-                        </a>
-                      </div>
-                    </div>
                   </dd>
                 </div>
               ))}
@@ -147,9 +136,12 @@ const ConnectedApps = () => {
                               <div className="bg-blue-600 h-2 rounded-full dark:bg-blue-500" style={{ width: `${app.usage}%` }}></div>
                             </div>
                           </td>
-                          <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                          <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 flex gap-x-2">
                             <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                              Edit<span className="sr-only">, {app.companytitle}</span>
+                              <TrashIcon className='w-5 h-5'/>
+                            </a>
+                            <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                              <PencilIcon className='w-5 h-5'/>
                             </a>
                           </td>
                         </tr>
