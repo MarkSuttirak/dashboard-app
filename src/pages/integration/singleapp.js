88be0c3c914a2e1f412dbc64fb1b22b15f747bac
MarkSuttirak <suttirak.ch@zaviago.com>
2023-11-13 16:53:46 +0700

    Commit Codes

--- src/pages/integration/singleapp.js
+++ src/pages/integration/singleapp.js
@@ -5,6 +5,7 @@ import { OpenInNewWindowIcon, PlusCircledIcon } from "@radix-ui/react-icons"
 import { Globe, Key, MessageSquare, Smile } from "lucide-react"
 import { Separator } from "src/components/ui/separator"
 import connectMarketingBg from "src/img/socialapp-bg.png"
+import installAppBg from "src/img/install-app-bg.png"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "src/components/ui/card"
 import {
   Dialog,
@@ -17,6 +18,8 @@ import {
   DialogTrigger,
 } from "src/components/ui/dialog"
 import { useState } from "react"
+import { AppStoreApp02, SidebarApp03, SidebarApp04 } from "src/components/sidebarApps"
+import ZaviagoIcon from "src/components/icon-menus/ZaviagoIcon"
 
 export default function SingleApp(){
   const { id } = useParams()
@@ -52,12 +55,33 @@ export default function SingleApp(){
                       Add to site
                     </Button>
                   </DialogTrigger>
-                  <DialogContent>
+                  <DialogContent className='p-0 max-w-[368px]'>
                     <DialogHeader>
-                      <DialogTitle>Are you sure absolutely sure?</DialogTitle>
-                      <DialogDescription>
-                        This action cannot be undone. This will permanently delete your account
-                        and remove your data from our servers.
+                      <div className='rounded-t-lg' style={{background:`url(${installAppBg})`,backgroundRepeat:"no-repeat",backgroundSize:"cover"}}>
+                        <DialogTitle className='flex justify-center gap-x-[55px] py-[50px]'>
+                          <div className="p-3 rounded-lg bg-white inline-block shadow-lg">
+                            <AppStoreApp02 />
+                          </div>
+                          <div className="p-3 rounded-lg bg-white inline-block shadow-lg">
+                            <SidebarApp03 width='52' height='52'/>
+                          </div>
+                        </DialogTitle>
+                      </div>
+                      <DialogDescription className='p-6'>
+                        <h1 className="secondary-heading tracking-[-0.4px] mb-3">Install addons app to your workspace</h1>
+                        <p>The app will be able to read the email address you use to log in with Zaviago.</p>
+
+                        <Button className='btn-with-icon text-[#006AFF] font-normal w-full mt-6 justify-start' variant='secondary'>
+                          <Key viewBox="0 0 24 24" width='16' height='16' color='#006AFF'/>
+                          Privacy Policy and Terms of Service.
+                        </Button>
+
+                        <div className="flex items-center justify-between mt-6">
+                          <DialogClose>
+                            <Button variant='outline'>Cancel</Button>
+                          </DialogClose>
+                          <Button>Install apps</Button>
+                        </div>
                       </DialogDescription>
                     </DialogHeader>
                   </DialogContent>
@@ -141,7 +165,7 @@ export default function SingleApp(){
 
       <section style={{background:`url(${connectMarketingBg})`,backgroundRepeat:"no-repeat",backgroundSize:"cover"}} className="rounded-xl p-6 flex gap-x-8">
         <div className="flex flex-col justify-between">
-          <article>
+          <article className="mt-4">
             <h1 className="secondary-heading mb-4">Connect Marketing place</h1>
             <p className="text-[13px] font-medium text-[#71717A] mt-4 mb-6">Say hello to the world and let readers know what your blog is all about.</p>
           </article>
