236a5e4fbd2d1f62e0e3a0ef44425df505f3ddce
MarkSuttirak <suttirak.ch@zaviago.com>
2023-12-07 16:02:02 +0700

    Commit Codes

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -123,11 +123,6 @@ export default function Sidebar({ loadingLogo, isSidebarOpen, setIsSidebarOpen }
         <div className={`nav-btns ${!isSidebarOpen ? 'show' : 'hide'}`} id="home-btn" onClick={() => setIsSidebarOpen(true)}>
           <ChevronsRight color='#18181B' viewBox='0 0 24 24' width='16' height='16' strokeWidth='1.5'/>
         </div>
-        <div className="nav-btns add-ons">
-          <Link to='/'>
-            <Icons.zaviagoApp />
-          </Link>
-        </div>
 
         <ServiceModals />
 
