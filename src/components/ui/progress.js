cb4136b07ae105bed561afe8eaf5838ef25874dc
MarkSuttirak <suttirak.ch@zaviago.com>
2023-12-20 12:10:25 +0700

    Update progress.js

--- src/components/ui/progress.js
+++ src/components/ui/progress.js
@@ -12,7 +12,7 @@ const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
     )}
     {...props}>
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
+      className="h-full w-full flex-1 bg-[#7000FF] transition-all rounded-md"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }} />
   </ProgressPrimitive.Root>
 ))
