60413fbaa2f5c7461ce4b290bef42edcad785f83
MarkSuttirak <suttirak.ch@zaviago.com>
2024-03-21 14:06:43 +0700

    Change the bg colour of upgrade pro btn on mobile

--- src/components/topbar/upgradeProButton.js
+++ src/components/topbar/upgradeProButton.js
@@ -10,12 +10,12 @@ const UpgradeProButton = () => {
   return (
     <UpgradeProModal triggerClassName='w-full md:w-fit px-5 py-3 md:p-0 bg-[#F6EFFF] md:bg-transparent mr-[3px]'>
       <div className='flex gap-x-2'>
-        <div className='whitespace-pre text-[13px] flex gap-x-[5px] items-center font-medium bg-accent pl-2 py-1 pr-1 rounded-md h-[34px]'>
-        {t('upgrade')}
-        <div className='flex bg-black rounded-md text-white cal-sans text-[13px] font-semibold h-full p-[6px] gap-x-[5px]'>
+        <div className='whitespace-pre text-[13px] flex gap-x-[5px] items-center font-medium bg-white md:bg-accent pl-2 py-1 pr-1 rounded-md h-[34px]'>
+          {t('upgrade')}
+          <div className='flex bg-black rounded-md text-white cal-sans text-[13px] font-semibold h-full p-[6px] gap-x-[5px]'>
             <UpgradeProIcon />
             {t('pro')}
-        </div>
+          </div>
         </div>
 
         <div className='flex items-center justify-between w-full md:hidden'>
