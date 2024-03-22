fbaed50bcec1b861d4c62ba93f6cddc575522010
MarkSuttirak <suttirak.ch@zaviago.com>
2024-03-22 14:24:55 +0700

    Translate 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,24 @@
 import { ArrowLeft } from "lucide-react";
 import { Button } from "../components/ui/button";
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 export default function NotFoundPage(){
-  document.title = "404 - Not Found"
+
+  const { t } = useTranslation()
+
+  document.title = t("not_found.title")
   const navigate = useNavigate()
   return (
     <div className="h-screen w-screen flex items-center justify-center flex-col gap-y-6 p-4 text-center">
       <div className="flex flex-col gap-y-4 items-center justify-center">
-        <h1 className="text-zinc-900 text-3xl font-semibold">404 - Page Not Found</h1>
-        <p className="text-zinc-500 text-lg font-medium">Sorry, the page you are looking for does not exist.</p>
+        <h1 className="text-zinc-900 text-3xl font-semibold">{t("not_found.title")}</h1>
+        <p className="text-zinc-500 text-lg font-medium">{t("not_found.desc")}</p>
       </div>
 
       <Button variant='ghost' className='btn-with-icon' onClick={() => navigate('/')}>
         <ArrowLeft viewBox='0 0 24 24' width='16' height='16' />
-        Go back to Home
+        {t("not_found.go_back")}
       </Button>
     </div>
   )
